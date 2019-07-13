bfd734bec4b1d7d91efb0fe12cb3b676269fe2ca
pablodiazt <pablodiaztr@gmail.com>
2019-07-13 16:19:59 -0500

    channges

--- read_breed_supplement_data.py
+++ read_breed_supplement_data.py
@@ -16,10 +16,11 @@ except:
 # In[2]:
 import pandas as pd
 import numpy as np
+psycopg2
 
 
 # In[3]:
-#Importing data from Vet Data
+# Importing data from Vet Data
 Vet_Data = pd.read_csv('/Users/pablodiaz/Downloads/l-dandy/work/task3/db-populator-csv/breed_supplement_matrix.csv')
 
 
@@ -28,5 +29,27 @@ Vet_Data
 
 
 #%%
-
+# Connnect to postgress db
+import psycopg2
+try:
+    connection = psycopg2.connect(user = "root",
+                                  password = "rootroot",
+                                  host = "127.0.0.1",
+                                  port = "5432",
+                                  database = "dandy_postgres_db")
+    cursor = connection.cursor()
+    # Print PostgreSQL Connection properties
+    print ( connection.get_dsn_parameters(),"\n")
+    # Print PostgreSQL version
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print("You are connected to - ", record,"\n")
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
+finally:
+    #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
 
