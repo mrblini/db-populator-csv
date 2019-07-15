94d702279cd3b5b2df36122b3b2fa9e6d01b4e80
pablodiazt <pablodiaztr@gmail.com>
2019-07-15 06:41:23 -0500

    added insert statements

--- db_data_populator.py
+++ db_data_populator.py
@@ -64,7 +64,20 @@ try:
     connection.commit()
     print("Table created successfully in PostgreSQL ")
     
-# ------- ERROR
+    
+    postgres_insert_query = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (%s,%s,%s)"""
+   record_to_insert = (5, 'One Plus 6', 950)
+   cursor.execute(postgres_insert_query, record_to_insert)
+   
+   connection.commit()
+   count = cursor.rowcount
+   print (count, "Record inserted successfully into mobile table")
+except (Exception, psycopg2.Error) as error :
+    if(connection):
+        print("Failed to insert record into mobile table", error)
+    
+    
+# ------------------ ERROR
 except (Exception, psycopg2.Error) as error :
     print ("Error while connecting to PostgreSQL", error)
 # ------- CLOSE CONNECTION 
