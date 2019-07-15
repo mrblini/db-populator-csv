beb03f71a0074be010c7014d61e5cff03e848c5f
pablodiazt <pablodiaztr@gmail.com>
2019-07-14 21:21:42 -0500

    changes

--- db_data_populator.py
+++ db_data_populator.py
@@ -7,7 +7,7 @@
 # ms-python.python added
 import os
 try:
-	os.chdir(os.path.join(os.getcwd(), '/Users/pablodiaz/Downloads/l-dandy/work/task3/db-populator-csv'))
+	os.chdir(os.path.join(os.getcwd(), '/Users/pablodiaz/Downloads/l-dandy/work/t4-csv_to_db/db-populator-csv'))
 	print(os.getcwd())
 except:
 	pass
@@ -16,12 +16,12 @@ except:
 # In[2]:
 import pandas as pd
 import numpy as np
-psycopg2
+import psycopg2
 
 
 # In[3]:
 # Importing data from Vet Data
-Vet_Data = pd.read_csv('/Users/pablodiaz/Downloads/l-dandy/work/task3/db-populator-csv/breed_supplement_matrix.csv')
+Vet_Data = pd.read_csv('/Users/pablodiaz/Downloads/l-dandy/work/t4-csv_to_db/db-populator-csv/breed_supplement_matrix.csv')
 
 
 # In[4]:
@@ -30,16 +30,16 @@ Vet_Data
 
 #%%
 # Connnect to postgress db
-import psycopg2
 try:
-    connection = psycopg2.connect(user = "root",
+    connection = psycopg2.connect(user = "pablodiaz",
                                   password = "rootroot",
                                   host = "127.0.0.1",
-                                  port = "5432",
-                                  database = "dandy_postgres_db")
+                                  port = "5400",
+                                  database = "pablodiaz")
     cursor = connection.cursor()
-    # Print PostgreSQL Connection properties
+    
     print ( connection.get_dsn_parameters(),"\n")
+    
     # Print PostgreSQL version
     cursor.execute("SELECT version();")
     record = cursor.fetchone()
@@ -52,4 +52,9 @@ finally:
             cursor.close()
             connection.close()
             print("PostgreSQL connection is closed")
-
+            
+            
+            
+            
+            
+            
