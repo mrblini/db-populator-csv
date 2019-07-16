0cb5bc19f65cef33c5abec339f9ca18b6453c922
pablodiazt <pablodiaztr@gmail.com>
2019-07-16 08:46:26 -0500

    fixed multi insert bug

--- db_data_populator.py
+++ db_data_populator.py
@@ -54,23 +54,19 @@ except (Exception, psycopg2.Error) as error :
 
 # -------------------------- CREATE TABLE 
 try:
-    #create_table_query = '''CREATE TABLE breed_supplement
-          #(breed_id INT PRIMARY KEY,
-          #breed_type TEXT NOT NULL,
-          #top_1_supplement TEXT,
-          #top_2_supplement TEXT,
-          #top_3_supplement TEXT,
-          #top_4_supplement TEXT,
-          #medical_issue_1 TEXT,
-          #medical_issue_2 TEXT,
-          #medical_issue_3 TEXT,
-          #medical_issue_4 TEXT); '''
-          
-    create_table_query = '''CREATE TABLE "del"
-          (id SERIAL PRIMARY KEY,
-          dogType VARCHAR(80) NOT NULL,
-          dogSupp VARCHAR(80),
-          dogFood VARCHAR(80)); '''
+    create_table_query = '''CREATE TABLE "breed_supplement"
+          (
+          id SERIAL PRIMARY KEY,
+          breed VARCHAR(80) NOT NULL,
+          top_1_supplement VARCHAR(100),
+          top_2_supplement VARCHAR(100),
+          top_3_supplement VARCHAR(100),
+          top_4_supplement VARCHAR(100),
+          medical_issue_1 VARCHAR(100),
+          medical_issue_2 VARCHAR(100),
+          medical_issue_3 VARCHAR(100),
+          medical_issue_4 VARCHAR(100)
+          ); '''
     
     cursor.execute(create_table_query)
     connection.commit()
@@ -84,20 +80,19 @@ except (Exception, psycopg2.DatabaseError) as error :
     
 # -------------------------- INSERT
 try:
-    #ALTER TABLE "del" ALTER COLUMN target
+    #ALTER TABLE table_name ALTER COLUMN col_name
     
     # --------- sql query
-    #postgres_insert_query = """ INSERT INTO breed_supplement (breed_type, top_1_supplement, top_2_supplement, top_3_supplement, top_4_supplement, medical_issue_1, medical_issue_2, medical_issue_3, medical_issue_4) VALUES ('eye supplement', 'hair supplement', 'liver supplement', 'heart supplement', 'eye fail', 'hair fail', 'liver fail', 'heart fail') """
-    sql_insert_query = """ INSERT INTO "del" (dogType, dogSupp, dogFood) VALUES ('a shietsu', 'a supplement', 'some food') """
-    another_sql_query = """ INSERT INTO "del" (dogType, dogSupp, dogFood) VALUES ('some shietsu', 'the supplement', 'the food') """
+    sql_insert_query = """ INSERT INTO "breed_supplement" (breed, top_1_supplement, top_2_supplement, top_3_supplement, top_4_supplement, medical_issue_1, medical_issue_2, medical_issue_3, medical_issue_4) VALUES ('shietsu', 'supplement one', 'supp two', 'supp 3', 'supp four', 'medical issue one', 'med issue 2', 'med 3', 'med 4') """
+    #another_sql_query = """ INSERT INTO "breed_supplement" (breed, top_1_supplement, top_2_supplement, top_3_supplement, top_4_supplement, medical_issue_1, medical_issue_2, medical_issue_3, medical_issue_4) VALUES ('bull dog', 'supplement one', 'supp two', 'supp 3', 'supp four', 'medical issue one', 'med issue 2', 'med 3', 'med 4') """
     #record_to_insert = ('bull dog', 'magnesium', 'croquets')
 
    
     # --------- execute sql query
-    #cursor.execute(sql_insert_query)
-    #cursor.execute(postgres_insert_query, record_to_insert)
+    cursor.execute(sql_insert_query)
     # --- executemany() to insert multiple rows rows
-    result = cursor.executemany(sql_insert_query, another_sql_query)
+    #result = cursor.executemany(sql_insert_query, another_sql_query)
+    #cursor.execute(postgres_insert_query, record_to_insert)
 
     
     # --------- commit
